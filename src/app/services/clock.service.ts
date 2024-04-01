cec3527fea7a169c87de13edabfb643545b0dd34
sandybombela <sandy.bombela@gmail.com>
2024-03-31 20:03:50 -0600

    navbar clock

--- src/app/services/clock.service.ts
+++ src/app/services/clock.service.ts
@@ -10,25 +10,22 @@ export class ClockService {
   // * Versión EventEmitter con más alcance
   // Permiten emisión de valores que van ocurriendo con el tiempo,
   // otros componentes pueden suscribirse
-  private clock$: Observable<Date>
+  private clock$: Observable<Date>;
 
-  // interval rxjs
-  // Función que crea un observable que emite datos en intervalos regulares
-  constructor() { 
-
+  // interval rxjs es una función que crea un observable que emite datos en intervalos regulares
+  constructor() {
     // map
     // Transformador de cada valor emitido
     this.clock$ = interval(1000).pipe(
       // map(() => new Date())
       map((intervalValue) => {
-        // console.log(intervalValue)
-        return new Date()
+        console.log(intervalValue)
+        return new Date();
       })
-    )
+    );
+  }
+  getClock(): Observable<Date> {
+    return this.clock$;
   }
 
-  getClock(): Observable<Date> {
-    return this.clock$
-  }
-  
 }
